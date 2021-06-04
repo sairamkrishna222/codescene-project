75c6177fcaa0138003eac8ef050f1fd876584a49
bhargav <bhargav@ed.com>
2021-06-04 14:46:55 +0530

    files related to Cloud-api-gateway

---
+++ Cloud-API-Gateway/src/main/java/com/srk/cloudapi/CloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.srk.cloudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Cloud-API-Gateway/src/test/java/com/srk/cloudapi/CloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srk.cloudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
